eed389e879fd626e6fe09f3fc9864d5d39d8192c
Pacôme Perrotin <pacome.perrotin@gmail.com>
2018-07-04 15:56:42 +0200

    Ajout des fichiers update.c et update.h qui visent à donner des fonctions optimisées pour gérer des mises à jours cycliques de listes d'objets... Plus d'info dans les commentaires des fichiers. Je vais sans doute pas aller plus loin avant qu'on puisse l'appliquer pour de bon dans un exemple.

--- dev/header/util_math.h
+++ dev/header/util_math.h
@@ -1,6 +1,32 @@
 #ifndef UTIL_MATH__H
 #define UTIL_MATH__H
 
-int negative(int n);
+/**
+* Par Pacôme le 04/07/2018
+* Ce fichier est là pour contenir toute fonction mathématique générale que nous
+* souhaiterions utiliser et qui n'est pas contenue dans la libraire de math de
+* C. Par exemple, des fonctions d'interpolation...
+*/
+
+/**
+* Prend une entrée sur [0, 1] et renvoie une valeur sur [0, 1]. Le plot de
+* cette fonction ralentit à l'approche de 0 et de 1 ; sert beaucoup pour les
+* animations.
+* f(x) = 6x^5 - 15x^4 + 10x^3
+*/
+
+double smootherInterpolation(double x);
+
+/**
+* f(x) = x^2
+*/
+
+double accelerationInterpolation(double x);
+
+/**
+* f(x) = 1 - (1-x)^2
+*/
+
+double decelerationInterpolation(double x);
 
 #endif
--- dev/src/util_math.c
+++ dev/src/util_math.c
@@ -1,5 +1,20 @@
 #include "util_math.h"
+#include <math.h>
 
-int negative(int n) {
-	return -n;
+double smootherInterpolation(double x) {
+	if( x <= 0 ) return 0;
+	if( x >= 1 ) return 1;
+	return 6 * pow(x, 5) - 15 * pow(x, 4) + 10 * pow(x, 3);
+}
+
+double accelerationInterpolation(double x) {
+	if( x <= 0 ) return 0;
+	if( x >= 1 ) return 1;
+	return x*x;
+}
+
+double decelerationInterpolation(double x) {
+	if( x <= 0 ) return 0;
+	if( x >= 1 ) return 1;
+	return 2*x - x*x;
 }
